584117e5373d680560e564d6dbb119630fe9081e
UminduH <73205708+UminduH@users.noreply.github.com>
2024-02-14 19:20:37 +0530

    Tutorial 03 - Answers

---
+++ Tutorial 03 - 14.02.2024/Question 01/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_01
+{
+    internal class Program
+    {
+        public class Course
+        {
+            private string courseName;
+            private string instructorName;
+            private double grade;
+
+            public string CourseName
+            {
+                get { return courseName; }
+            }
+
+            public double Grade
+            {
+                get { return grade; }
+            }
+
+            public void SetInstructorName(string nameOfInstructor)
+            {
+                if (string.IsNullOrEmpty(nameOfInstructor))
+                {
+                    throw new ArgumentException("Instructor name cannot be empty !");
+                }
+
+                instructorName = nameOfInstructor;
+            }
+
+            private string CalculateLetterGrade()
+            {
+                if (grade >= 75)
+                {
+                    return "A";
+                }
+                else if (grade >= 60)
+                {
+                    return "B";
+                }
+                else if (grade >= 50)
+                {
+                    return "C";
+                }
+                else if (grade >= 40)
+                {
+                    return "D";
+                }    
+                else
+                {
+                    return "F";
+                }    
+            }
+
+            public void PrintCourseInfo()
+            {
+                Console.WriteLine($"\nCourse Name: {courseName}");
+                Console.WriteLine($"Instructor Name: {instructorName}");
+                Console.WriteLine($"Grade: {CalculateLetterGrade()}");
+            }
+
+            public Course(string courseName, string instructorName, double grade)
+            {
+                if (string.IsNullOrEmpty(courseName))
+                {
+                    throw new ArgumentException("Course name cannot be empty !");
+                }
+
+                this.courseName = courseName;
+
+                SetInstructorName(instructorName);
+
+                if (grade < 0 || grade > 100)
+                {
+                    throw new ArgumentOutOfRangeException("Grade must be between 0 and 100 !");
+                }
+
+                this.grade = grade;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Course Name: ");
+            string courseName = Console.ReadLine();
+
+            Console.WriteLine("\nEnter Instructor Name: ");
+            string instructorName = Console.ReadLine();
+
+            Console.WriteLine("\nEnter Marks: ");
+            double marks = Convert.ToDouble(Console.ReadLine());
+
+            Course myCourse = new Course(courseName, instructorName, marks);
+            myCourse.PrintCourseInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tutorial 03 - 14.02.2024/Question 02/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_02
+{
+    internal class Program
+    {
+        public class Employee
+        {
+            private int employeeID;
+            private string fullName;
+            private double salary;
+
+            public int EmployeeID
+            {
+                get { return employeeID; }
+            }
+
+            public string FullName
+            {
+                get { return fullName; }
+                set { fullName = value; }
+            }
+
+            public double Salary
+            {
+                get { return salary; }
+            }
+
+            public Employee(int empId, string empName, double empSalary)
+            {
+                employeeID = empId;
+                fullName = empName;
+                salary = empSalary;
+            }
+
+            public void DisplayEmployeeInfo()
+            {
+                Console.WriteLine($"\nEmployee ID: {EmployeeID}");
+                Console.WriteLine($"Employee Name: {FullName}");
+                Console.WriteLine($"Employee Salary: ${Salary}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(101, "John Doe", 50000);
+
+            Console.WriteLine($"Employee ID: {employee.EmployeeID}");
+
+            Console.WriteLine("\nCurrent Employee Information: ");
+            employee.DisplayEmployeeInfo();
+
+            employee.FullName = "John Smith";
+
+            // employee.Salary = 100000;
+            // Since the salary property is read-only, it cannot be modified.
+            // If you try to modify it, it will result in a compilation error.
+
+            Console.WriteLine("\nUpdated Employee Information: ");
+            employee.DisplayEmployeeInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
