15bf201e17ea879463b42c03063fc76bc12f14a7
UminduH <73205708+UminduH@users.noreply.github.com>
2024-02-21 21:13:01 +0530

    Tutorial 03 - Questions 3 & 4

---
+++ Tutorial 03 - 14.02.2024/Question 03/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_03
+{
+    public class Product
+    {
+        private int productId;
+        private string productName;
+        private double price;
+        private int quantityInStock;
+
+        public Product(int productId, string productName, double price, int quantityInStock)
+        {
+            this.productId = productId;
+            this.productName = productName;
+            this.price = price;
+            this.quantityInStock = quantityInStock;
+        }
+
+        public void AddProduct(int quantityToAdd)
+        {
+            if (quantityToAdd > 0)
+            {
+                quantityInStock += quantityToAdd;
+                Console.WriteLine($"{quantityToAdd} {productName}(s) added successfully. New Stock Quantity: {quantityInStock}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid quantity to add!");
+            }
+        }
+
+        public void BuyProduct(int quantityToBuy)
+        {
+            if (quantityToBuy > 0 && quantityToBuy <= quantityInStock)
+            {
+                Console.WriteLine($"{quantityToBuy} {productName}(s) bought successfully. Price: ${price * quantityToBuy}");
+                quantityInStock -= quantityToBuy;
+            }
+            else
+            {
+                Console.WriteLine($"Invalid quantity to buy or insufficient stock.");
+            }
+        }
+
+        public int ProductId
+        {
+            get { return productId; }
+        }
+
+        public string ProductName
+        {
+            get { return productName; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+        }
+
+        public int QuantityInStock
+        {
+            get { return quantityInStock; }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product laptop = new Product(101, "Laptop", 800, 10);
+
+            Console.WriteLine("Product Details: ");
+            Console.WriteLine($"Product ID: {laptop.ProductId}");
+            Console.WriteLine($"Product Name: {laptop.ProductName}");
+            Console.WriteLine($"Product Price: {laptop.Price}");
+            Console.WriteLine($"Product Stock: {laptop.QuantityInStock}");
+
+            // laptop.ProductId = 199;
+            // Since the productId property is read-only, it cannot be modified.
+            // If you try to modify it, it will result in a compilation error.
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tutorial 03 - 14.02.2024/Question 04/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_04
+{
+    public class Shape
+    {
+        protected string shapeType;
+        protected double area;
+
+        public void CalculateArea(double val1, double val2)
+        {
+            area = val1 * val2;
+        }
+
+        public virtual void DisplayShapeInfo()
+        {
+            Console.WriteLine($"Shape Type: {shapeType}");
+            Console.WriteLine($"Area: {area}");
+        }
+
+        public Shape()
+        {
+            shapeType = "Default Shape";
+            area = 0;
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        private double length;
+        private double width;
+
+        public void SetDimensions(double length, double width )
+        {
+            this.length = length;
+            this.width = width;
+
+            shapeType = "Rectangle";
+            CalculateArea(length, width);
+        }
+
+        public override void DisplayShapeInfo()
+        {
+            base.DisplayShapeInfo();
+            Console.WriteLine($"Length: {length}");
+            Console.WriteLine($"Width: {width}");
+        }
+    }
+
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public void SetRadius(double radius)
+        {
+            this.radius = radius;
+            shapeType = "Circle";
+            CalculateArea(radius, radius);
+            area *= Math.PI;
+            area = Math.Round(area, 2);
+        }
+
+        public override void DisplayShapeInfo()
+        {
+            base.DisplayShapeInfo();
+            Console.WriteLine($"Radius: {radius}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.SetDimensions(5, 3);
+            rectangle.DisplayShapeInfo();
+
+            Console.WriteLine();
+
+            Circle circle = new Circle();
+            circle.SetRadius(4);
+            circle.DisplayShapeInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
